63f8e0943ab79655f885263bc3960f0998456851
Kruti Manani <Kmanani465@rku.ac.in>
2026-04-20 17:21:13 +0530

    feat: add Firebase packages and initialize in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,9 +6,11 @@ import 'package:skill_exchange/config/di/providers.dart';
 import 'package:skill_exchange/config/router/app_router.dart';
 import 'package:skill_exchange/core/theme/app_theme.dart';
 import 'package:skill_exchange/core/widgets/connectivity_banner.dart';
+import 'package:skill_exchange/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   final prefs = await SharedPreferences.getInstance();
 
   runApp(
